75d9d253fa49623d8ccdeaadd2cd058d5c9d1cbf
Pranshu1902 <pranshu1902@gmail.com>
2022-04-23 03:00:38 +0530

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://f398a8b12f134efb8962d45b6d8ce0ea@o1215845.ingest.sentry.io/6357665",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 /*
 const heading = React.createElement('div', { className: 'text-5xl font-bold text-center' }, 'Hello World');
